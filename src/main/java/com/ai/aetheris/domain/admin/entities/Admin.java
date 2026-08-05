1b832edb07ba5731ad0cbc7f822a2341fa43a4de
hasunB <hasunbandara17@gmail.com>
2026-08-05 22:23:42 +0530

    chore: update project dependencies and lockfile

---
+++ src/main/java/com/ai/aetheris/domain/admin/entities/Admin.java
@@ -0,0 +1,57 @@
+package com.ai.aetheris.domain.admin.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "admins")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "company", nullable = false)
+    private String company;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "postal_code", nullable = false)
+    private String postalCode;
+
+    @Column(name = "country", nullable = false)
+    private String country;
+
+    @Column(name = "phone", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "ip_address", nullable = false)
+    private String ipAddress;
+
+    @Column(name = "created", nullable = false)
+    private LocalDateTime created;
+
+    @Column(name = "updated", nullable = false)
+    private LocalDateTime updated;
+
+}
